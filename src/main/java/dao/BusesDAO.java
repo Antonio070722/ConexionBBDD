fe22e911bea728458ddc041b1eef61094909e334
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2026-01-24 23:21:07 +0100

    Mas cositas, hecho el DAO de Bus, BusesDAO.java

--- src/main/java/dao/BusesDAO.java
+++ src/main/java/dao/BusesDAO.java
@@ -1,4 +1,35 @@
 package dao;
 
+import DB.ConexionBBDD;
+import model.Bus;
+
+import java.sql.Connection;
+import java.sql.*;
+
 public class BusesDAO {
+    public Bus consultarBus(int registro){
+        String sql = "SELECT Registro, Licencia, Tipo FROM BUS WHERE Registro = ?";
+
+        try(Connection con = ConexionBBDD.getConexion();
+        PreparedStatement ps = con.prepareStatement(sql)){
+
+            ps.setInt(1, registro);
+
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+                    String Registro = rs.getString("Registro");
+                    String Licencia = rs.getString("Licencia");
+                    String Tipo = rs.getString("Tipo");
+                    return new Bus(Integer.parseInt(Registro), Licencia, Tipo);
+                }else {
+                    return null; // No se encontró el bus
+                }
+            }catch (SQLException ex) {
+                throw new RuntimeException("Error al extraer los datos de la Base de Datos", ex);
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException("Error al consultar bus", e);
+        }
+    }
 }
--- src/main/java/model/Bus.java
+++ src/main/java/model/Bus.java
@@ -5,6 +5,42 @@ public class Bus {
     private String Licencia;
     private String Tipo;
 
+    public Bus(int registro, String licencia, String tipo) {
+        Registro = registro;
+        Licencia = licencia;
+        Tipo = tipo;
+    }
 
+    public int getRegistro() {
+        return Registro;
+    }
 
+    public void setRegistro(int registro) {
+        Registro = registro;
+    }
+
+    public String getLicencia() {
+        return Licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        Licencia = licencia;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "Registro=" + Registro +
+                ", Licencia='" + Licencia + '\'' +
+                ", Tipo='" + Tipo + '\'' +
+                '}';
+    }
 }
